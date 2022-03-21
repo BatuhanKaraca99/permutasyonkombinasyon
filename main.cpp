ab730510fe92e600168ef5fa15763f0d420b31c1
Batuhan KARACA <47457138+BatuhanKaraca99@users.noreply.github.com>
2022-03-21 18:29:53 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n,k,b,f=1,t=1,z=1;
+  cout << "Permutasyon ve Kombinasyon Hesaplayici. \n\n";
+  cout << "---------------------------------\n";
+  cout << "n değerini giriniz: ";
+  cin >> n;
+  cout << "k değerini giriniz: ";
+  cin >> k;
+  
+  for(int a=1;a<=n;a++){
+    f *= a;
+  }
+
+  for(int b=1;b<=(n-k);b++){
+    t *= b;
+  }
+
+  for(int c=1;c<=k;c++){
+    z *= c;
+  }
+   cout << "Permutasyon Sonucu: " << f/t << endl;
+   cout << "Kombinasyon Sonucu: " << f/(z*t);
+} 
